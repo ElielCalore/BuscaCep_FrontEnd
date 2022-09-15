cfc0baf7c8bfff89959c725866f562a221f6bcb1
eliel <elieldscalore@gmail.com>
2022-09-15 00:03:20 -0300

    functionality ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-
-
+import {useState } from "react";
+import {SearchBar} from "../src/components/search/index";
 
 function App() {
+  const [search, setSearch] = useState();
+  const [cep, setCep] = useState({
+    localidade: "",
+    logradouro: "",
+    cep: "",
+    uf:"",
+    ibge:"",
+    ddd:""
+  });
+  
   return (
     <div>
+      <div className="search">
+        <SearchBar search={search} setSearch={setSearch} setCep={setCep} cep={cep} />
+      </div>
+      <div>
+        <h1>LOCALIDADE: {cep.localidade}</h1>
+        <p>LOGRADOURO: {cep.logradouro}</p>
+        <p>CEP: {cep.bairro}</p>
+        <p>UF: {cep.uf}</p>
+        <p>IBGE: {cep.ibge}</p>
+        <p>DDD: {cep.ddd}</p>
+      </div>
       
     </div>
   );
---
+++ src/components/search/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import axios from "axios";
+
+export function SearchBar({ search, setCep, setSearch}) {
+
+  function handleChange(e) {
+    setSearch(e.target.value);
+  }
+
+  async function handleSubmit(e) {
+        try{
+          const response= await axios.get(`http://localhost:3001/cep/read/${search}`);
+          setCep(response.data)
+        }
+      catch (error) {
+          console.log(error);
+          alert("CEP INVALIDO");
+      }
+  }
+
+  return (
+    <>
+      <div className="col-lg-6">
+          <div className="input-group">
+           <input onChange={handleChange} match={"/^[0-9]{5}-[0-9]{3}$/"} maxLength="8" type="number" className="form-control" placeholder="Entre com a informação aqui..."/>
+           <span className="input-group-btn">
+            <button onClick={handleSubmit} className="btn btn-default" type="button">Pesquise</button>
+           </span>
+          </div>
+      </div>
+
+      {/*}
+      <input
+        size="large"
+        value={search}
+        type="number"
+        placeholder="input search text"
+        onChange={handleChange}
+        enterButton
+      >
+      </input>
+      <button>Pesquisar</button>
+      */}
+    </>
+  );
+}
