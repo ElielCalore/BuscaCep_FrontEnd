7dc1208a4140347c02490f2bfe41b258c9ba2399
eliel <elieldscalore@gmail.com>
2022-09-18 10:33:19 -0300

    refactor: update readme

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://vtrinacep.herokuapp.com/",
+    baseURL: "http://localhost:3001/"
+  //baseURL: "https://vtrinacep.herokuapp.com/",
 });
