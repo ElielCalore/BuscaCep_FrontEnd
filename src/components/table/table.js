ddf4cdc30ebcc48dd150a7088774babc5e5d2e4f
eliel <elieldscalore@gmail.com>
2022-09-18 10:51:35 -0300

    refactor: update readme and images add new image update color text in search component

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,7 +1,7 @@
 export function Table({ cep, messageBody }) {
   return (
     <div className=" d-flex flex-column justify-content-around w-75">
-      <div className="bg-primary p-2 text-white bg-opacity-50 d-flex flex-row justify-content-between">
+      <div className="bg-dark h5 p-2 text-white d-flex flex-row justify-content-between">
         {messageBody.map((current) => {
           return (
             <div key={current.info} className="m-1">
@@ -10,7 +10,7 @@ export function Table({ cep, messageBody }) {
           );
         })}
       </div>
-      <div className="bg-light p-2 text-dark bg-opacity-50 d-flex flex-row justify-content-between align-items-center p-1 ">
+      <div className="bg-light p-2 text-dark d-flex flex-row justify-content-between align-items-center p-1 ">
         <div>{cep.logradouro !== "" ? cep.logradouro : " not found "} </div>
         <div>{cep.bairro !== "" ? cep.bairro : "not found"}</div>
         <div>{cep.localidade}</div>
